a8d595c6e42adfe817efec8432a4dd573ab5d9a5
Álvaro Tejero-Cantero <alvaro@teje.ro>
2023-10-10 10:16:09 +0200

    Add function to print GitHub repo owner.

--- src/MyStatsPackage.jl
+++ src/MyStatsPackage.jl
@@ -1,6 +1,10 @@
 module MyStatsPackage
 
-greet() = print("Hello World!")
-include("statistics_functions.jl")
-export rse_mean, rse_std, rse_tstat, StatResult
+    function printOwner()
+        println("alvorithm")
+    end
+
+
+    include("statistics_functions.jl")
+    export rse_mean, rse_std, rse_tstat, StatResult
 end # module MyStatsPackage
