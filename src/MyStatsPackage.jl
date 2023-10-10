634a0ec949cff70a85f3f51164eee8b5dfe07240
Álvaro Tejero-Cantero <alvaro@teje.ro>
2023-10-10 14:57:35 +0200

    Export also rse_sum, so that it works for tests w/o qualification.

--- src/MyStatsPackage.jl
+++ src/MyStatsPackage.jl
@@ -5,6 +5,6 @@ function printOwner()
 end
 
 
-    include("statistics_functions.jl")
-    export rse_mean, rse_std, rse_tstat, StatResult
+include("statistics_functions.jl")
+export rse_sum, rse_mean, rse_std, rse_tstat, StatResult
 end # module MyStatsPackage
